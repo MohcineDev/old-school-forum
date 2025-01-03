07c86571765144a66951861c745ee9ca3c9e2846
MohcineDev <mouhcinedev@gmail.com>
2025-01-03 19:07:07 +0100

    create footer component

---
+++ public/components/footerCmp.js
@@ -0,0 +1,14 @@
+export default function footerCmp(){
+    return `
+        <div class="container">
+            <ul class="footer-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Terms of Service</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+            </ul>
+        </div>
+        <p>&copy; 2024 Classic Forum. All rights reserved.</p>
+    `
+}
